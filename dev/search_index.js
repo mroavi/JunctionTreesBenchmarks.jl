5c75557c062806ad2a0f677099b75708d9459e33
Documenter.jl <documenter@juliadocs.github.io>
2022-09-15 22:05:44 +0000

    build based on eecb550

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JunctionTreesBenchmarks","category":"page"},{"location":"#JunctionTreesBenchmarks","page":"Home","title":"JunctionTreesBenchmarks","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JunctionTreesBenchmarks.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JunctionTreesBenchmarks]","category":"page"}]
+}
